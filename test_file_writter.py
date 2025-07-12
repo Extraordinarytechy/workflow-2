360bc049c12dbbf13cb6938e6718fc67f39597de
Extraordinarytechy <extraordinarytechy@gmail.com>
2025-07-12 20:52:53 +0530

    Fix:spelling fix error not found issue in test_file_writter

--- test_file_writter.py
+++ test_file_writter.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from file_writer import write_to_file
+from file_writter import write_to_file
 
 def test_write_to_file():
     test_filename = "test_output.txt"
